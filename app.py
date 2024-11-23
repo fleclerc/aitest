2110e00d17b7fdd1b53f41571b3841e9e8d61e80
Francis Leclerc <francis.leclerc@horizontrading.io>
2024-11-22 21:16:08 -0500

    test

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import gradio as gr
 def greet(name): return f"Hello {name}!"
 print("starting")
 try:
-    gr.Interface(fn=greet, inputs="text", outputs="text")
+    demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+    demo.launch()
 except Exception as ex:
     print ("ex:" + str(ex))
 print ("after")
